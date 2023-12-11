59907269760ce066a8eb5f24e651479847d605a8
Raj <raj.pulapakura@gmail.com>
2023-12-11 23:24:20 +1100

    ❌ removed main module

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1' 
+VERSION = '0.1.0' 
 DESCRIPTION = 'Opinionated performance plotting for TensorFlow models.'
 LONG_DESCRIPTION = 'Opinionated performance plotting for TensorFlow models.'
 
